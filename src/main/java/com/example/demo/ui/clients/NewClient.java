01dc27afa99a16401e158d348cd040286ca06a08
dino460 <raphael.z.cali@gmail.com>
2023-12-10 15:49:03 -0300

    Javadocs incoming

--- src/main/java/com/example/demo/ui/clients/NewClient.java
+++ src/main/java/com/example/demo/ui/clients/NewClient.java
@@ -32,6 +32,11 @@ public class NewClient extends VerticalLayout {
     TextField mobileNumber = new TextField("Mobile Number");
     boolean clientExists;
 
+    /**
+     * Constructor to the UI of the client page
+     * 
+     * @param clientService
+     */
     public NewClient(ClientService clientService) {
         H1 Title = new H1("New Client");
 
@@ -68,6 +73,12 @@ public class NewClient extends VerticalLayout {
         
     }
 
+    /**
+     * Function to verify if the a client exists
+     * 
+     * @param clientService
+     * @param client
+     */
     public void verifyClient(ClientService clientService, ClientEntity client) {
         clientService.findAll().forEach(clientEntity -> {
             if (client.getCpf().equals(clientEntity.getCpf())) {
@@ -76,6 +87,13 @@ public class NewClient extends VerticalLayout {
         });
     }
 
+    /**
+     * Function to test if a given new ClientEntity is valid by testing if it already exists
+     * 
+     * @param clientService
+     * @param client
+     * @return true if it does not exist and is valid, false otherwise
+     */
     public boolean clientIsValid(ClientService clientService, ClientEntity client) {
         verifyClient(clientService, client);
 
