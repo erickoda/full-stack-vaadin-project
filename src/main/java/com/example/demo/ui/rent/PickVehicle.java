434cec9d72c6de8a98f1c06b98b08fcb2053f2f2
dino460 <raphael.z.cali@gmail.com>
2023-12-10 22:46:34 -0300

    Added notification to pick vehicles & fixed date not changing rentValue

--- src/main/java/com/example/demo/ui/rent/PickVehicle.java
+++ src/main/java/com/example/demo/ui/rent/PickVehicle.java
@@ -14,6 +14,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -107,6 +108,7 @@ public class PickVehicle extends VerticalLayout {
 
             chooseRent.setStatus(RentStatus.EFFECTED);
             rentService.update(chooseRent);
+            Notification.show("Vehicle successfully taken");
         });
 
         back.addClickListener(event -> {
