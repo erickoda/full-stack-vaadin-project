f7abdf52fdc37ae57c8c65dfffaaab5e50d5b907
Erick Oda Coulter <erickodac@hotmail.com>
2023-12-10 23:48:06 -0300

    docs: operation entity docs

--- src/main/java/com/example/demo/backend/operational/OperationalEntity.java
+++ src/main/java/com/example/demo/backend/operational/OperationalEntity.java
@@ -41,30 +41,69 @@ public class OperationalEntity
     @NotNull
     private int insuranceDailyValue;
 
+    /**
+     * Returns the tier of the vehicle
+    */
     public VehicleTier getTier() {
         return this.tier;
     }
 
+    /**
+     * Gets the daily rent
+     * 
+     * @return      the daily rent
+     * @see         Integer
+    */
     public int getDailyRent() {
         return this.dailyRent;
     }
 
+    /**
+     * Gets the fuel fill value
+     * 
+     * @return      the fuel fill value
+     * @see         Integer
+    */
     public int getFuelFillValue() {
         return this.fuelFillValue;
     }
 
+    /**
+     * Gets the Exterior cleaning value
+     * 
+     * @return      the exterior cleaning value
+     * @see         Integer
+    */
     public int getExteriorCleaningValue() {
         return this.exteriorCleaningValue;
     }
 
+    /**
+     * Gets the Interior cleaning value
+     * 
+     * @return      the Interior cleaning value
+     * @see         Integer
+    */
     public int getInteriorCleaningValue() {
         return this.interiorCleaningValue;
     }
 
+    /**
+     * Gets the Insurance Daily Value
+     * 
+     * @return      the Insurance Daily Value
+     * @see         Integer
+    */
     public int getInsuranceDailyValue() {
         return this.insuranceDailyValue;
     }
 
+    /**
+     * Checks if the operational entity is valid
+     * 
+     * @return      a boolean that represents if the entity is valid
+     * @see         boolean
+    */
     public boolean check() {
         if (
             this.tier == null || this.dailyRent < 0 || this.fuelFillValue < 0 ||
