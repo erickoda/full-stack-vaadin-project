6dc006df0f87760e6927e82a8f699de539611c5d
Erick Oda Coulter <erickodac@hotmail.com>
2023-12-10 21:26:29 -0300

    refactor: removed unused imports

--- src/main/java/com/example/demo/ui/reports/ReportsMainView.java
+++ src/main/java/com/example/demo/ui/reports/ReportsMainView.java
@@ -1,25 +1,15 @@
 package com.example.demo.ui.reports;
 
-import org.vaadin.reports.PrintPreviewReport;
-
 import com.example.demo.backend.client.ClientService;
 import com.example.demo.backend.rent.RentService;
-import com.example.demo.backend.vehicle.VehicleEntity;
 import com.example.demo.backend.vehicle.VehicleService;
 import com.example.demo.backend.vehicle.VehicleStatus;
 import com.example.demo.backend.vehicle.VehicleTier;
 import com.example.demo.ui.MainLayout;
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.charts.Chart;
-import com.vaadin.flow.component.charts.model.ChartType;
-import com.vaadin.flow.component.charts.model.DataSeries;
-import com.vaadin.flow.component.charts.model.DataSeriesItem;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.H4;
-import com.vaadin.flow.component.html.H5;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
